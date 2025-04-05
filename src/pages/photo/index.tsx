bd34a0a64817392b548a8f91d915f25d5eab34ea
Justino Soares <justinocsoares123@gmail.com>
2025-04-06 00:55:30 +0100

    register danger zones

--- src/pages/photo/index.tsx
+++ src/pages/photo/index.tsx
@@ -1,5 +1,5 @@
-import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
-import { useRef, useState } from 'react';
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera'
+import { useEffect, useRef, useState } from 'react'
 import {
   Image,
   Button,
@@ -7,108 +7,217 @@ import {
   Text,
   TouchableOpacity,
   View,
-  Alert
-} from 'react-native';
-import * as MediaLibrary from 'expo-media-library';
-import mudar_camera from '../../assets/mudar-camera.png';
-import foto from '../../assets/foto.png';
+  Alert,
+  ActivityIndicator
+} from 'react-native'
+import * as MediaLibrary from 'expo-media-library'
+import mudar_camera from '../../assets/mudar-camera.png'
+import foto from '../../assets/foto.png'
+import bom from '../../assets/bom.png'
+import mal from '../../assets/mal.png'
+import out from '../../assets/out.png'
+import repeat from '../../assets/repeat.png'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import * as Location from 'expo-location'
+import { style } from '../login/style'
 
-export default function App() {
-  const [facing, setFacing] = useState<CameraType>('back');
-  const [photo, setPhoto] = useState<string | null>(null);
-  const [permission, requestPermission] = useCameraPermissions();
-  const [galleryPermission, requestGalleryPermission] = MediaLibrary.usePermissions();
-  const cameraRef = useRef<any>(null);
+export default function App ({navigation}: any) {
+  const [facing, setFacing] = useState<CameraType>('back')
+  const [photo, setPhoto] = useState<string | null>(null)
+  const [loading, setLoading] = useState(false)
+  const [permission, requestPermission] = useCameraPermissions()
+  const [galleryPermission, requestGalleryPermission] =
+    MediaLibrary.usePermissions()
+  const cameraRef = useRef<any>(null)
+
+  useEffect(() => {
+    // verificar se o usuário tem permissão para estar nessa tela
+    const checkPermission = async () => {
+      const token = await AsyncStorage.getItem('Token')
+      if (!token) {
+        Alert.alert('Erro', 'Você não tem permissão para acessar essa tela')
+        // Redirecionar para a tela de login ou outra ação
+        navigation.navigate('Login');
+        return
+      }
+    }
+    checkPermission()
+  }, []);
+
+
 
   // Verificar e solicitar permissões da galeria
   if (!galleryPermission) {
-    requestGalleryPermission();
+    requestGalleryPermission()
   }
 
   if (!permission) {
-    return <View />;
+    return <View />
   }
 
   if (!permission.granted) {
     return (
       <View style={styles.container}>
-        <Text style={styles.message}>Precisamos da sua permissão para acessar a câmera</Text>
-        <Button onPress={requestPermission} title="Conceder permissão" />
+        <Text style={styles.message}>
+          Precisamos da sua permissão para acessar a câmera
+        </Text>
+        <Button onPress={requestPermission} title='Conceder permissão' />
       </View>
-    );
+    )
   }
 
-  function toggleCameraFacing() {
-    setFacing(current => (current === 'back' ? 'front' : 'back'));
+  function toggleCameraFacing () {
+    setFacing(current => (current === 'back' ? 'front' : 'back'))
   }
 
-  async function takePicture() {
+  async function takePicture () {
     if (cameraRef.current) {
-      const photoData = await cameraRef.current.takePictureAsync();
-      setPhoto(photoData.uri);
+      const photoData = await cameraRef.current.takePictureAsync()
+      setPhoto(photoData.uri)
     }
   }
 
-  function discardPhoto() {
-    setPhoto(null);
+  function discardPhoto () {
+    setPhoto(null)
   }
 
-  async function saveToGallery() {
+  async function saveToGallery () {
     if (photo) {
       try {
-        await MediaLibrary.createAssetAsync(photo);
-        Alert.alert('Sucesso', 'Foto salva na galeria!');
+        await MediaLibrary.createAssetAsync(photo)
+        Alert.alert('Sucesso', 'Foto salva na galeria!')
       } catch (error) {
-        Alert.alert('Erro', 'Não foi possível salvar a foto.');
+        Alert.alert('Erro', 'Não foi possível salvar a foto.')
       }
     }
   }
 
-  async function uploadToCloudinary() {
-    if (!photo) return;
+  async function RegisterDangerZone () {
+    // pegar a latitude e longitude exata do local que o telefone está
+    setLoading(true)
+    const { status } = await Location.requestForegroundPermissionsAsync()
+    if (status !== 'granted') {
+      Alert.alert('Erro', 'Permissão de localização negada.')
 
-    const data = new FormData();
+      return
+    }
+    if (!photo) return
+    const data = new FormData()
     data.append('file', {
       uri: photo,
       type: 'image/jpeg',
       name: 'photo.jpg'
-    } as any);
-    data.append('upload_preset', 'mapazzz'); // Defina seu upload_preset no Cloudinary
+    } as any)
+    data.append('upload_preset', 'mapazzz') // Defina seu upload_preset no Cloudinary
 
     try {
-      let response = await fetch('https://api.cloudinary.com/v1_1/da8q1jhdf/image/upload', {
-        method: 'POST',
-        body: data
-      });
+      let response = await fetch(
+        'https://api.cloudinary.com/v1_1/da8q1jhdf/image/upload',
+        {
+          method: 'POST',
+          body: data
+        }
+      )
 
-      let result = await response.json();
-      if (result.ok) {
-        // console.log('Upload bem-sucedido:', result);
-        Alert.alert('Upload Concluído', `Imagem enviada: ${result}`);
-      }
-      else {
-        Alert.alert('Erro', result);
+      let result = await response.json()
+      if (response.ok) {
+        // Alert.alert('Sucesso', 'Imagem enviada com sucesso!');
+        const token = await AsyncStorage.getItem('Token')
+        try {
+          const location = await Location.getCurrentPositionAsync({})
+          const latitude = location.coords.latitude
+          const longitude = location.coords.longitude
+          let responseApi = await fetch(
+            'https://mapazzz.onrender.com/api/danger_zone/register',
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${token}`
+              },
+              body: JSON.stringify({
+                lat: latitude,
+                lon: longitude,
+                image: result.secure_url
+              })
+            }
+          )
+          let responseData = await responseApi.json()
+
+          if (responseApi.ok) {
+            Alert.alert('Sucesso', 'Zona de perigo registrada com sucesso!')
+          } else {
+            Alert.alert('Erro', responseData.message)
+            setLoading(false)
+            console.log('Erro ao enviar a imagem:', responseData)
+          }
+        } catch (error) {
+          Alert.alert('Erro', 'Não foi possível salvar a imagem na galeria.')
+          setLoading(false)
+        }
+      } else {
+        Alert.alert('Erro', 'Falha ao enviar a imagem.')
+        setLoading(false)
       }
     } catch (error) {
-      Alert.alert('Erro', 'Falha ao enviar a imagem para o Cloudinary.');
+      // Alert.alert('Erro', 'Falha ao enviar a imagem para o Cloudinary.');
+      Alert.alert('Error', 'Falha a reportar a zona de risco!')
+      setLoading(false)
+    } finally {
+      setLoading(false)
     }
   }
 
+
+
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.topLeftLogo} onPress={toggleCameraFacing}>
-        <Image source={mudar_camera} style={styles.smallLogo} />
-      </TouchableOpacity>
+      {!photo && (
+        <TouchableOpacity
+          style={styles.topLeftLogo}
+          
+        >
+          <Image source={out} style={styles.out} />
+          <TouchableOpacity onPress={toggleCameraFacing}>
+
+          <Image  source={repeat} style={styles.changeCamera}  />
+          
+          </TouchableOpacity>
+        </TouchableOpacity>
+      )}
 
       {photo ? (
-        <View style={styles.previewContainer}>
-          <Image source={{ uri: photo }} style={styles.previewImage} />
-          <View style={styles.buttonContainer}>
-            <Button title="Descartar" onPress={discardPhoto} />
-            <Button title="Salvar na Galeria" onPress={saveToGallery} color="red" />
-            <Button title="Enviar para Cloud" onPress={uploadToCloudinary} color="green" />
-          </View>
-        </View>
+        <>
+          {loading ? (
+            <View style={styles.loadingContainer}>
+              <ActivityIndicator size="large" color="#7F1734" />
+              <Text style={styles.message}>Aguarde um pouco...</Text>
+              {/* <Text style={styles.message}>Carregando...</Text> */}
+            </View>
+          ) : (
+            <View style={styles.previewContainer}>
+              <Image source={{ uri: photo }} style={styles.previewImage} />
+              <View style={styles.buttonContainer}>
+                <View style={styles.buttonContainerchild}>
+                  <TouchableOpacity
+                    style={styles.button}
+                    onPress={discardPhoto}
+                  >
+                    <Image source={mal} style={styles.descarte} />
+                  </TouchableOpacity>
+                  {/* <Button title="Salvar na Galeria" onPress={saveToGallery} color="red" /> */}
+
+                  <TouchableOpacity
+                    style={styles.button}
+                    onPress={RegisterDangerZone}
+                  >
+                    <Image source={bom} style={styles.report} />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+          )}
+        </>
       ) : (
         <CameraView ref={cameraRef} style={styles.camera} facing={facing}>
           <View style={styles.buttonContainer}>
@@ -119,7 +228,7 @@ export default function App() {
         </CameraView>
       )}
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -127,9 +236,29 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center'
   },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  descarte: {
+    padding: 10,
+    width: 45,
+    height: 45,
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  report: {
+    padding: 10,
+    width: 40,
+    height: 40,
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
   message: {
     textAlign: 'center',
-    paddingBottom: 10
+    paddingBottom: 10,
+    fontWeight: 'bold',
   },
   camera: {
     flex: 1
@@ -141,16 +270,37 @@ const styles = StyleSheet.create({
     bottom: 30,
     width: '100%'
   },
+  buttonContainerchild: {
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 10,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '60%',
+    alignSelf: 'center'
+  },
   button: {
     alignSelf: 'center'
   },
   topLeftLogo: {
-    position: 'absolute',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 15,
+    gap: 10,
+    marginHorizontal: 10,
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+    height: 50,
     top: 40,
-    right: 10,
     zIndex: 1
   },
-  smallLogo: {
+  out: {
+    width: 20,
+    height: 20,
+    resizeMode: 'contain'
+  },
+  changeCamera: {
     width: 30,
     height: 30,
     resizeMode: 'contain'
@@ -161,15 +311,16 @@ const styles = StyleSheet.create({
     resizeMode: 'contain'
   },
   previewContainer: {
+    backgroundColor: '#000',
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'black'
+    alignItems: 'center'
   },
   previewImage: {
     flex: 1,
+    // marginTop: 70,
     width: '100%',
     height: '100%',
     borderRadius: 10
   }
-});
+})
